2fcbfdeee2e181eb3245dca2eab019e6adad03da
Imygo <a97852003@gmail.com>
2025-05-16 13:35:41 +0900

    Week2 문제 cpp 파일 수정

--- Junyeong/Week2/1541.cpp
+++ Junyeong/Week2/1541.cpp
@@ -8,11 +8,18 @@ using namespace std;
 int main()
 {
     string expression,number;
+    // "-" 체크를 하는 이유
+    // "-" 연산자가 나왔을 경우 그 후에 나오는 피연산자들을 모두 뺀다면 가장 작은 값을 얻을 수 있다.
+    // 예시1) 55 - 50 + 40 -> 55 - (50 + 40) = -35 "-" 후에 나타나는 값들을 모두 결과값에서 뺀다면 결과값 도출 가능
+    // 예시2) 30 - 10 - 70 + 40 - 30 -> 30 - 10 - (70 + 40) - 30 = -120
+    
     bool minus_check=false;
     int answer=0;
 
     cin>>expression;
     
+    // 시간복잡도 : 주어진 문자열의 길이가 N 이라고 했을 때 -> O(N)
+
     for(int i=0;i<=expression.length();i++)
     {
         if(expression[i]=='+'||expression[i]=='-'||i==expression.length())
@@ -28,10 +35,15 @@ int main()
                 number="";
             }
         }
+
+        //number라는 변수에 문자들을 합치다가  연산자를 탐색했을 경우 정수로 변환한다.
+        //이미 사용된 숫자들을 제거하기 위해 number=""를 통해 초기화 시킨다.
+
         else
         {
             number+=expression[i];
         }
+     
         
         if(expression[i]=='-')
         {
--- Junyeong/Week2/1654.cpp
+++ Junyeong/Week2/1654.cpp
@@ -7,12 +7,15 @@ using namespace std;
 
 int main()
 {
-    long long int N,K,mx=0,start,end,count,result=0;
+    long long int N,K,mx=0;
+    long long int start,end,count,result=0;
     
     //만약 가장 긴 길이가 2^31-1일 경우 오버플로우가 발생한다.
     long long int mid;
     vector<int> lan;
     cin>>K>>N;
+
+    //가지고 있는 랜선의 길이를 저장한다.
     for(int i=0;i<K;i++)
     {
         int length;
@@ -24,7 +27,10 @@ int main()
         }
     }
 
-    //검색범위 : 1 ~ mx (가장 긴 길이를 가진 랜선)
+    // 검색범위 : 1 ~ mx (가장 긴 길이를 가진 랜선)
+    // 최악의 경우 : 주어진 최소길이가 1이고 최대길이가 2^31-1이며, 이미 가지고 있는 랜선의 개수가 10,000
+    // 순차탐색을 통해 답을 산출하려면 (2^31-1) * 10,000(=K) 약 21,474,836,470,000로 2초안에 해결할 수 없어보인다.
+    // 이분탐색을 통해 답을 산출할 경우 log2(2^31-1) * 10,000(=K) 약 320,000정도의 연산으로 해결할 수 있다. O(Klog(주어진 랜선의 최대길이))
     start=1;
     end=mx;
     count=0;
