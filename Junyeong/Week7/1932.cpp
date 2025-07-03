f977143eb79606cbc44cb18a0b1d182c1dd528b0
Imygo <a97852003@gmail.com>
2025-07-04 08:02:18 +0900

    7주차 문제풀이 업로드

---
+++ Junyeong/Week7/1932.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+vector<int> input_v[500];
+vector<int> mid_sum[500];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> N;
+
+    // 1번째는 값이 하나 2번째줄은 값이 두개 3번째는 3개...
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < i + 1; j++) {
+            int num;
+            cin >> num;
+            input_v[i].push_back(num);
+        }
+    }
+
+    // 첫번째 줄에 대해선 값을 처리할 필요 없음
+    // 초기값만 넣어주는 것으로 해결된다.
+    mid_sum[0].push_back(input_v[0][0]);
+
+    for (int i = 1; i < N; i++) {
+        // 양 끝값들에 대해선 합을 비교할 필요 없다
+        for (int j = 0; j < input_v[i].size(); j++) {
+            if (j == 0) {
+                mid_sum[i].push_back(mid_sum[i - 1][0] + input_v[i][j]);
+            }
+            else if (j == input_v[i].size() - 1) {
+                mid_sum[i].push_back(mid_sum[i - 1][j - 1] + input_v[i][j]);
+            }
+            else {
+                //중간에 끼인 위치라면 앞선 누적합의 값을 비교할 필요가 있다.
+                mid_sum[i].push_back(max(mid_sum[i - 1][j - 1], mid_sum[i - 1][j]) + input_v[i][j]);
+            }
+        }
+    }
+
+    int mx = -1;
+    for (int i = 0; i < N; i++) {
+        if (mid_sum[N - 1][i] > mx) {
+            mx = mid_sum[N - 1][i];
+        }
+    }
+
+    cout << mx;
+
+    return 0;
+}
---
+++ Junyeong/Week7/1987.cpp
@@ -0,0 +1,56 @@
+// 1987 알파벳
+#include <iostream>
+
+using namespace std;
+
+// 0-25번 인덱스는 차례대로 A부터 Z를 나타냄 0이면 처리안한 알파벳
+bool alphabets[27];
+int R, C, result;
+int dx[4] = { 0,0,-1,1 };
+int dy[4] = { 1,-1,0,0 };
+// 좌상단을 1행 1열이라고 했기 때문에 이것에 맞춰서 설정
+char grid[21][21];
+
+// 현재 방문중인 좌표의 위치와 지나온 칸의 수를 전달함
+void solution(pair<int,int>location,int count) {
+    alphabets[grid[location.first][location.second] - 'A'] = true;
+    count++;
+
+    //상하좌우중에 방문할 수 있는 좌표확인
+    for (int i = 0; i < 4; i++) {
+        int nx = location.first + dx[i];
+        int ny = location.second + dy[i];
+
+        if (nx <= 0 || nx > C || ny <= 0 || ny > R)continue;
+        if (alphabets[grid[nx][ny] - 'A'])continue;
+        
+        cout << '\n';
+        solution({ nx,ny }, count);
+    }
+
+    // 인접한 좌표들 중에 방문할 수 있는 알파벳이 존재하지 않는다면 최대의 칸수 비교
+    // 그리고 현재 방문한 알파벳을 방문하지 않았다는 후처리 필요
+    if (count > result)result = count;
+    alphabets[grid[location.first][location.second] - 'A'] = 0;
+}
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> R >> C;
+
+    for (int i = 1; i <= R; i++) {
+        for (int j = 1; j <= C; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    solution({1,1}, 0);
+
+    cout << result;
+
+    return 0;
+}
